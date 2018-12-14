7571ba462cd15b0b37a7cedb6df180978c6a7d23
Sergio <breakpoint.software@gmail.com>
2018-12-14 17:11:53 -0300

    webpack azure adal

---
+++ dist/index.js
@@ -0,0 +1,18 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactDom = require("react-dom");
+
+var _reactDom2 = _interopRequireDefault(_reactDom);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+window.AuthenticationContext = require("./adal");
+exports.default = window.AuthenticationContext;
---
+++ src/index.js
@@ -0,0 +1,4 @@
+window.AuthenticationContext = require("./adal");
+import React from "react";
+import ReactDOM from "react-dom";
+export default window.AuthenticationContext;
